a2a615bc774a6e53ec4f995f03b31224ed592616
Maya Driver <mayadrivermd@gmail.com>
2014-11-21 23:37:18 +0000

    fixing indentations

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,79 +1,79 @@
 describe("Thermostat", function(){
-	var thermostat;
+  var thermostat;
 
-	beforeEach(function() {
-		thermostat = new Thermostat();
-	});
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
-	it("should start at 20 degrees", function(){
-		expect(thermostat.temperature).toEqual(20);
-	});
+  it("should start at 20 degrees", function(){
+    expect(thermostat.temperature).toEqual(20);
+  });
 
-	it("can increase the temperature", function(){
-		thermostat.increaseTemp();
-		expect(thermostat.temperature).toEqual(21);
-	});
+  it("can increase the temperature", function(){
+    thermostat.increaseTemp();
+    expect(thermostat.temperature).toEqual(21);
+  });
 
-	it("can decrease the temperature", function(){
-		thermostat.decreaseTemp();
-		expect(thermostat.temperature).toEqual(19);
-	});
+  it("can decrease the temperature", function(){
+    thermostat.decreaseTemp();
+    expect(thermostat.temperature).toEqual(19);
+  });
 
-	it("has a minimum temp of 10 degrees", function(){
-		thermostat.temperature = 10;
-		thermostat.decreaseTemp();
-		expect(thermostat.temperature).toEqual(10);
-	});
+  it("has a minimum temp of 10 degrees", function(){
+    thermostat.temperature = 10;
+    thermostat.decreaseTemp();
+    expect(thermostat.temperature).toEqual(10);
+  });
 
-	it("has a powersaving mode that is on by default", function(){
-		expect(thermostat.psm).toBe(true);
-	});
+  it("has a powersaving mode that is on by default", function(){
+    expect(thermostat.psm).toBe(true);
+  });
 
-	it("has a max temp of 25 when in psm", function(){
-		thermostat.psm = true;
-		expect(thermostat.maxTemp()).toEqual(25);
-	});
+  it("has a max temp of 25 when in psm", function(){
+    thermostat.psm = true;
+    expect(thermostat.maxTemp()).toEqual(25);
+  });
 
-	it("has a max temp of 32 when not in psm", function(){
-		thermostat.psm = false;
-		thermostat.temperature = 32;
-		thermostat.increaseTemp();
-		expect(thermostat.maxTemp()).toEqual(32);
-	});
+  it("has a max temp of 32 when not in psm", function(){
+    thermostat.psm = false;
+    thermostat.temperature = 32;
+    thermostat.increaseTemp();
+    expect(thermostat.maxTemp()).toEqual(32);
+  });
 
-	it("can reset the temperature", function(){
-		thermostat.temperature = 25;
-		thermostat.resetTemp();
-		expect(thermostat.temperature).toEqual(20);
-	});
+  it("can reset the temperature", function(){
+    thermostat.temperature = 25;
+    thermostat.resetTemp();
+    expect(thermostat.temperature).toEqual(20);
+  });
 
-	it("should have a low energy usage if less than 18 degrees", function(){
-		thermostat.temperature = 17;
-		expect(thermostat.energyUsage()).toEqual('Low');
-	});
+  it("should have a low energy usage if less than 18 degrees", function(){
+    thermostat.temperature = 17;
+    expect(thermostat.energyUsage()).toEqual('Low');
+  });
 
-	it("should have a medium energy usage if between 18 and 25 degrees", function(){
-		thermostat.temperature = 18;
-		expect(thermostat.energyUsage()).toEqual('Medium');
-		thermostat.temperature = 24;
-		expect(thermostat.energyUsage()).toEqual('Medium');
-	});
+  it("should have a medium energy usage if between 18 and 25 degrees", function(){
+    thermostat.temperature = 18;
+    expect(thermostat.energyUsage()).toEqual('Medium');
+    thermostat.temperature = 24;
+    expect(thermostat.energyUsage()).toEqual('Medium');
+  });
 
-	it("should have a high energy useage if more than 25 degrees", function(){
-		thermostat.temperature = 25;
-		expect(thermostat.energyUsage()).toEqual('High');
-	});
+  it("should have a high energy useage if more than 25 degrees", function(){
+    thermostat.temperature = 25;
+    expect(thermostat.energyUsage()).toEqual('High');
+  });
 
-	it("can switch off powersaving mode", function(){
-		thermostat.psmToggle();
-		expect(thermostat.psm).toBe(false);
-	});
+  it("can switch off powersaving mode", function(){
+    thermostat.psmToggle();
+    expect(thermostat.psm).toBe(false);
+  });
 
-	it("can switch on powersaving mode when reset", function(){
-		thermostat.psmToggle();
-		thermostat.resetTemp();
-		expect(thermostat.psm).toBe(true);
-	});
+  it("can switch on powersaving mode when reset", function(){
+    thermostat.psmToggle();
+    thermostat.resetTemp();
+    expect(thermostat.psm).toBe(true);
+  });
 
 });
 
--- src/interface.js
+++ src/interface.js
@@ -1,49 +1,49 @@
 $(document).ready(function(){
 
-	var t = new Thermostat();
+  var t = new Thermostat();
 
-	$('#up').on('click', function(){
-		$('#temp').replaceWith('<p id="temp">' + t.increaseTemp() + '&deg;C</p>');
-		tempColours();
-	});
+  $('#up').on('click', function(){
+    $('#temp').replaceWith('<p id="temp">' + t.increaseTemp() + '&deg;C</p>');
+    tempColours();
+  });
 
-	$('#down').on('click', function(){
-		$('#temp').replaceWith('<p id="temp">' + t.decreaseTemp() + '&deg;C</p>');
-		tempColours();
-	});
+  $('#down').on('click', function(){
+    $('#temp').replaceWith('<p id="temp">' + t.decreaseTemp() + '&deg;C</p>');
+    tempColours();
+  });
 
-	function tempColours() {
-		if(t.energyUsage() === 'High') $('#temp').css('color', 'red');
-		else if(t.energyUsage() === 'Medium') $('#temp').css('color', 'orange');
-		else $('#temp').css('color', 'green');
-	};
+  function tempColours() {
+    if(t.energyUsage() === 'High') $('#temp').css('color', 'red');
+    else if(t.energyUsage() === 'Medium') $('#temp').css('color', 'orange');
+    else $('#temp').css('color', 'green');
+  };
 
-	$("#psm").on('click', function(){
-  	if($(this).hasClass('fa-check-square-o')){
-  		$(this).removeClass('fa-check-square-o').addClass('fa-square-o');}
-  	else {
-  		$(this).removeClass('fa-square-o').addClass('fa-check-square-o');}
+  $("#psm").on('click', function(){
+    if($(this).hasClass('fa-check-square-o')){
+      $(this).removeClass('fa-check-square-o').addClass('fa-square-o');}
+    else {
+      $(this).removeClass('fa-square-o').addClass('fa-check-square-o');}
   });
 
   $("#psm").on('click', savingOff);
 
-	function savingOff() {
-	    t.psmToggle();
-	    $('#psm').html("Switch on PowerSaving Mode");
-	    $("#psm").off('click').on('click', savingOn)
-	};
+  function savingOff() {
+      t.psmToggle();
+      $('#psm').html("Switch on PowerSaving Mode");
+      $("#psm").off('click').on('click', savingOn)
+  };
 
-	function savingOn() {
-	    t.psmToggle();
-	    $('#psm').html("Switch off PowerSaving Mode");
-	    $("#psm").off('click').on('click', savingOff)
-	};
+  function savingOn() {
+      t.psmToggle();
+      $('#psm').html("Switch off PowerSaving Mode");
+      $("#psm").off('click').on('click', savingOff)
+  };
 
-	$('#reset').on('click', function(){
-		t.resetTemp();
-		$('#temp').replaceWith('<p id="temp">' + t.temperature + '&deg;C</p>');
-	  $('#psm').prop('checked', true);
-	});
+  $('#reset').on('click', function(){
+    t.resetTemp();
+    $('#temp').replaceWith('<p id="temp">' + t.temperature + '&deg;C</p>');
+    $('#psm').prop('checked', true);
+  });
 
 });
 
